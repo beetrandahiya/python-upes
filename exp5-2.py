39875d23ef40245dcd7ef8ae73fbc3cd00b7a419
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2022-03-03 10:39:58 +0530

    Update exp5-2.py

---
+++ exp5-2.py
@@ -0,0 +1,30 @@
+
+def check_baggage(bw):
+    if(bw>=0 and bw<=40):
+        return True
+    else:
+        return False
+
+
+def check_immigration(ey):
+    if(ey>=2030 and ey<=2050):
+        return True
+    else:
+        return False
+
+def check_security(noc):
+    if(noc.lower()=='valid'):
+        return True
+    else:
+        return False
+
+def traveler(t_id,t_name,bw,ey,noc):
+    if(check_baggage(bw) and check_immigration(ey) and check_security(noc)):
+        print(t_id)
+        print(t_name)
+        print("Allow Traveler to fly!")
+    else:
+        print(t_id)
+        print(t_name)
+        print("Detain Traveler for re-checking!")
+
