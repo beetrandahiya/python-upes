83992272101cc11f1891d73201338a140c1c6196
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2022-03-10 14:23:20 +0530

    exp7-1

--- exp7-1.py
+++ exp7-1.py
@@ -1,3 +1,15 @@
 import os
-file1=open("exp7-1.txt","w")
+file1=open("abc.txt","r")
+a=file1.readlines()
+nstr=""
 
+for j in a:
+    for i in j:
+        if i=='"':
+            nstr+=' \\"'
+        else:
+            nstr+=i
+
+print(nstr)
+file2=open("abc1.txt",'w')
+file2.write(nstr)
