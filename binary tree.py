b2329d415587ed7b36a696776574893d9d64dd17
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2022-03-21 21:58:37 +0530

    Update binary tree.py

--- binary tree.py
+++ binary tree.py
@@ -19,6 +19,29 @@ class BinaryTree:
         else:
             self.value = value
 
+    def InOrderTraversal(self, root):
+        res=[]
+        if root:
+            res=self.InOrderTraversal(root.left)
+            res.append(root.value)
+            res=res+self.InOrderTraversal(root.right)
+        return res
+
+    def PreOrderTraversal(self, root):
+        res=[]
+        if root:
+            res.append(root.value)
+            res+=self.PreOrderTraversal(root.left)
+            res=res+self.PreOrderTraversal(root.right)
+        return res
+    def PostOrderTraversal(self, root):
+        res=[]
+        if root:
+            res=self.PostOrderTraversal(root.left)
+            res=res+self.PostOrderTraversal(root.right)
+            res.append(root.value)
+        return res
+
     def PrintTree(self):
         if self.left:
             self.left.PrintTree()
@@ -48,3 +71,4 @@ Tree would look like:
 
 '''
 root.PrintTree()
+print(root.PostOrderTraversal(root))
