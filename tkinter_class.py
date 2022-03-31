8b562c90faabb0a57a2726016d2fd81b1dd22a16
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2022-03-31 13:17:28 +0530

    Update tkinter_class.py

--- tkinter_class.py
+++ tkinter_class.py
@@ -2,14 +2,17 @@ from tkinter import *
 root = Tk()
 root.geometry("400x300")
 root.title("Tkinter Class")
-def myclick():
-    mylabel1.config(text="I have been clicked!")
-    mylabel2.config(text="I have been clicked again!")
-mylabel1 = Label(root, text="Hello World!")
-mylabel2 = Label(root, text="Hello World again!")
-mybutton = Button(root, text="Click Me!", command=myclick)
-mylabel1.grid(row=0,column=0)
-mylabel2.grid(row=1,column=1)
-mybutton.grid(row=2,column=2)
-root.mainloop()
 
+my_menu = Menu(root)
+root.config(menu=my_menu)
+def command():
+    my_label=Label(root,text="You clicked")
+
+#create menu item
+file_menu = Menu(my_menu)
+my_menu.add_cascade(label="File",menu=file_menu)
+file_menu.add_separator()
+file_menu.add_command(label="New",command=command)
+file_menu.add_command(label="Exit",command=root.quit)
+
+root.mainloop()
