349914ea9d3c69827cfaf88104276a2b3ea587e3
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2022-04-07 23:18:58 +0530

    for branideads

--- ass-2-2.py
+++ ass-2-2.py
@@ -1,16 +1,17 @@
 import os
 
+# comments are for braindeads
 
-f=open("abc.txt","r")
-a=f.readlines()
-nstr=""
+f=open("abc.txt","r")    #open file
+a=f.readlines()          #read all the lines and store it in an array
+nstr=""                  #make an empty string
 
 for i in range(len(a)):
     if(i%2!=0):
-        nstr+=a[i]
+        nstr+=a[i]       #add odd lines to the empty string
 
-f2=open("abc2.txt","w")
+f2=open("abc2.txt","w")  # write emty string to new file
 f2.write(nstr)
 
-f.close()
+f.close()                # close both files
 f2.close()
