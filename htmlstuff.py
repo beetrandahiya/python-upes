c4b6b50ab536d00bf4a778c22f6ec9c94f12f6d4
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2022-04-07 15:17:09 +0530

    Update htmlstuff.py

--- htmlstuff.py
+++ htmlstuff.py
@@ -7,3 +7,20 @@ print("<font size=+1>Environment</font>")
 
 for param in os.environ.keys():
     print("<br> %20s = %s" % (param, os.environ[param]))
+
+import cgi, cgitb
+
+form=cgi.FieldStorage()
+
+firstname=form.getvalue('firstname')
+lastname=form.getvalue('lastname')
+
+print("content-type: text/html\r\n\r\n")
+print("<html>")
+print("<head>")
+print("<title>Hello - Second CGI Program</title>")
+print("</head>")
+print("<body>")
+print("<h1>Hello %s %s</h1>" % (firstname, lastname))
+print("</body>")
+print("</html>")
