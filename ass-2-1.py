60588ae46b53d88602d240f9ebd42a767f4d8ea9
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2022-04-07 23:08:43 +0530

    for dumbfucks

--- ass-2-1.py
+++ ass-2-1.py
@@ -1,1 +1,14 @@
+# for brainers
+
 print("".join(input().split(":")))
+
+
+# for dumbfucks
+
+a=input()       # input string
+
+a=a.split(":")  # split the string into parts and store them into an array
+
+a="".join(a)    # join the elements to make a string
+
+print(a)
