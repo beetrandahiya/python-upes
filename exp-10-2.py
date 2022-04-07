a1a31d60d7076d084920011b1c13719ed0a64d46
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2022-04-07 10:13:03 +0530

    Create exp-10-2.py

---
+++ exp-10-2.py
@@ -0,0 +1,22 @@
+class Vehicle:
+    def __init__(self,name,max_speed,mileage):
+        self.name=name
+        self.max_speed=max_speed
+        self.mileage=mileage
+    
+    def seating_capacity(self,capacity):
+        return f"The seating capacity of a {self.name} is {capacity} passengers."
+
+
+
+class Bus(Vehicle):
+    def __init__(self,name, max_speed, mileage):
+        super().seating_capacity(50)
+    
+
+Buss=Vehicle('Pinky',100,10)
+
+# display stuff
+
+print(Buss.max_speed)
+print(Buss.mileage)
