ab07546f231384440461a819bb9671828cd1d285
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2022-04-07 13:32:00 +0530

    Update htmlstuff.py

--- htmlstuff.py
+++ htmlstuff.py
@@ -1,9 +1,9 @@
+import os 
+
+
 print("content-type: text/html\r\n\r\n")
-print("<html>")
-print("<head>")
-print("<title>Hello World - First CGI Program</title>")
-print("</head>")
-print("<body>")
-print("<h2>Hello World! This is my first CGI program</h2>")
-print("</body>")
-print("</html>")
+
+print("<font size=+1>Environment</font>")
+
+for param in os.environ.keys():
+    print("<br> %20s = %s" % (param, os.environ[param]))
