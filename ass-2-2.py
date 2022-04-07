7888ac32a1f63ab5275ee8fc88c3f3fb2306e105
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2022-04-07 23:32:03 +0530

    .

--- ass-2-2.py
+++ ass-2-2.py
@@ -2,13 +2,13 @@ import os
 
 # comments are for braindeads
 
-f=open("abc.txt","r")    #open file
-a=f.readlines()          #read all the lines and store it in an array
-nstr=""                  #make an empty string
+f=open("abc.txt","r")    # open file
+a=f.readlines()          # read all the lines and store it in an array
+nstr=""                  # make an empty string
 
 for i in range(len(a)):
     if(i%2!=0):
-        nstr+=a[i]       #add odd lines to the empty string
+        nstr+=a[i]       # add odd lines to the empty string
 
 f2=open("abc2.txt","w")  # write emty string to new file
 f2.write(nstr)
