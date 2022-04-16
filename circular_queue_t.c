2364c20e5dd7d7b7bd03a50159e605eabccbfff8
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2022-04-16 23:41:09 +0530

    Update circular_queue_t.c

--- circular_queue_t.c
+++ circular_queue_t.c
@@ -46,6 +46,48 @@ void display(){
         printf("Queue is empty\n");
     }
     else{
-       
+        if(front<=rear){
+            for(int i=front;i<=rear;i++){
+                printf("%d ",items[i]);
+            }
+            printf("\n----------------\n");
+        }
+        else{
+            for(int i=front;i<MAX;i++){
+                printf("%d ",items[i]);
+            }
+            for(int i=0;i<=rear;i++){
+                printf("%d ",items[i]);
+            }
+            printf("\n----------------\n");
+        }
+    }
+}
+
+void main(){
+    while(true){
+        printf("1. Enqueue\n2. Dequeue\n3. Display\n4. Exit\n");
+        int choice;
+        scanf("%d",&choice);
+        switch(choice){
+            case 1:
+                printf("Enter the element to be enqueued\n");
+                int item;
+                scanf("%d",&item);
+                enqueue(item);
+                break;
+            case 2:
+                dequeue();
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                exit(0);
+                break;
+            default:
+                printf("Invalid choice\n");
+                break;
+        }
     }
 }
