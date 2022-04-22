bbb1e75dfd0e537807337edcfd0c7dffc224b09e
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2022-04-22 09:28:27 +0530

    Update binary_tree.c

--- binary_tree.c
+++ binary_tree.c
@@ -8,3 +8,50 @@ typedef struct node{
     struct node* right;
 }node;
 
+void makeTree(node** head, int data){
+    node* newNode = (node*)malloc(sizeof(node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    if(*head == NULL){
+        *head = newNode;
+    }else{
+        node* temp = *head;
+        while(temp->left != NULL){
+            temp = temp->left;
+        }
+        temp->left = newNode;
+    }
+}
+
+void insert(node** head, int data){
+    node* newNode = (node*)malloc(sizeof(node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    if(*head == NULL){
+        *head = newNode;
+    }else{
+        node* temp = *head;
+        if(data < temp->data){
+            while(temp->left != NULL){
+                temp = temp->left;
+            }
+            temp->left = newNode;
+        }else{
+            while(temp->right != NULL){
+                temp = temp->right;
+            }
+            temp->right = newNode;
+        }
+    }
+}
+
+void printTree(node* head){
+    if(head == NULL){
+        return;
+    }
+    printf("%d ", head->data);
+    printTree(head->left);
+    printTree(head->right);
+}
