3f4d63d1aa73c5ce56bcefea3979b07ec5d3b5ab
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2022-04-24 21:34:29 +0530

    ques 2 , exp

--- linked_list.c
+++ linked_list.c
@@ -4,26 +4,65 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-struct node{
-    int data;
-    struct node *next;
-};
+#include<stdio.h>
+#include<stdlib.h>
+//make linked lists
 
-typedef struct node node;
+typedef struct node{
+    int data;
+    struct node* next;
+}node;
+
+void makeList(node** head, int data){
+    node* newNode = (node*)malloc(sizeof(node));
+    newNode->data = data;
+    newNode->next = NULL;
+    if(*head == NULL){
+        *head = newNode;
+    }else{
+        node* temp = *head;
+        while(temp->next != NULL){
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+
+void printList(node* head){
+    if(head == NULL){
+        return;
+    }
+    printf("|%d|--->", head->data);
+    printList(head->next);
+}
+
+//recursive linear search
+void search(node* head, int data){
+    if(head == NULL){
+        return;
+    }
+    if(head->data == data){
+        printf("%d found\n", data);
+        return;
+    }
+    search(head->next, data);
+}
 
 int main(){
-    node *first ;
-    node *second ;
-    node *third ;
 
-    first = (node*)malloc(sizeof(node));
-    second = (node*)malloc(sizeof(node));
-    third = (node*)malloc(sizeof(node));
-
-    first->data = 1;
-    first->next = second;
-    
-    second->data = 2;
-
-    printf("%d\n",first->next->data);
+    node* head = NULL;
+    makeList(&head, 1);
+    makeList(&head, 2);
+    makeList(&head, 3);
+    makeList(&head, 4);
+    makeList(&head, 5);
+    makeList(&head, 6);
+    makeList(&head, 7);
+    makeList(&head, 8);
+    makeList(&head, 9);
+    makeList(&head, 10);
+    printList(head);
+    printf("\n");
+    search(head, 5);
+    return 0;
 }
